d56b5a8344051405f7fe84cefd3f7657b7f825fb
RAEES KHAN <inforaees6908090@gmail.com>
2025-02-17 11:27:24 -0800

    use product to js

--- amazon.js/amzzon.js
+++ amazon.js/amzzon.js
@@ -1,28 +1,4 @@
-const products=[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        star:4.5,
-        count:87
-    },
-    priceCents:1090
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    rating:{
-        star:4,
-        count:127
-    },
-    priceCents:2095
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        star:4.5,
-        count:56
-    },
-    priceCents:799
-}];
+
 let productsHTML='';
 products.forEach((product)=>{
   productsHTML+=`    <div class="product-container">
@@ -37,12 +13,10 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star *10}.png">
+        src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
-
+              ${product.rating.count} </div>
+              </div>
           <div class="product-price">
             ${(product.priceCents/100).toFixed(2)}
           </div>
@@ -73,6 +47,5 @@ products.forEach((product)=>{
           </button>
         </div>`;
 });
-let js=document.querySelector('.js-product-grid').innerHTML=productsHTML
-
+document.querySelector('.js-product-grid').innerHTML=productsHTML
 
