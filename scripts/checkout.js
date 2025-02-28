405e7e30e1747dba9b1962eaff5d68e253a3947b
RAEES KHAN <inforaees6908090@gmail.com>
2025-02-27 22:35:08 -0800

    Delet the product from Html when click on delet

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,8 @@ cart.forEach((cartItem)=>{
 
 cartSummaryHtml+=
 
-    `<div class="cart-item-container">
+    `<div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -98,6 +99,9 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
    const productId= link.dataset.productId
    removeformcart(productId)
-   console.log(cart)
+const container=document.querySelector(
+    `.js-cart-item-container-${productId}`
+)
+container.remove();
 })
 })
