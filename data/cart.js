77923c665eae38038e7401c1542e2f655d6e39c8
RAEES KHAN <inforaees6908090@gmail.com>
2025-02-28 23:10:36 -0800

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,17 @@
-export let cart=[{
-  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
-},{
- productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
- quantity:1
-}];
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2
+  },{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1
+  }];  
+}
+export
+function savetolocalstorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 
 //This code is code is for productIt to addtocart //
@@ -23,6 +30,7 @@ export function addtocart(productId){
         quantity:1
       })
     }
+    savetolocalstorage()
   }
 
  export function removeformcart(productId){
@@ -33,5 +41,6 @@ export function addtocart(productId){
           }
     })
     cart=newCart
+    savetolocalstorage()
   }
 
